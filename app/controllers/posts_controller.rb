3eee830eeb1235caa43b7cad498b81cb6cb72503
Geovanna RC <gechro@outlook.com>
2015-11-16 18:33:42 -0500

    Add basic styling for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,26 @@
 class PostsController < ApplicationController
 
-def index
-end
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    if @post.save
+      redirect_to posts_path
+    else
+      redirect_to new_post_path
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
 
 end
